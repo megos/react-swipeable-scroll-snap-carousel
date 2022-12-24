cc49de26f414a77ff1d12491f92ed6f5d43fd410
megos <gmegos.27@gmail.com>
2022-12-24 11:47:42 +0900

    Add vanillaExtractPlugin

--- vite.config.page.ts
+++ vite.config.page.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    vanillaExtractPlugin(),
   ],
   base: 'react-swipeable-scroll-snap-carousel',
   publicDir: false,
