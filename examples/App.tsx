88453c4e5bd03d02d3262c090d5bf3e2fd21ee53
megos <gmegos.27@gmail.com>
2022-12-24 14:56:11 +0900

    Add prev / next buttons

--- examples/App.tsx
+++ examples/App.tsx
@@ -6,6 +6,7 @@ const items = (
     <div style={{ backgroundColor: 'pink' }}>1</div>
     <div style={{ backgroundColor: 'skyblue' }}>2</div>
     <div style={{ backgroundColor: 'limegreen' }}>3</div>
+    <div style={{ backgroundColor: 'grey' }}>4</div>
   </>
 )
 
@@ -18,10 +19,16 @@ function App() {
           {items}
         </Carousel>
       </div>
-      <div className="buttons">
+      <div className="buttons center">
+        <button onClick={() => setIndex(prev => prev - 1)} disabled={index === 0}>&lt;</button>
         <button onClick={() => setIndex(0)}>1</button>
         <button onClick={() => setIndex(1)}>2</button>
         <button onClick={() => setIndex(2)}>3</button>
+        <button onClick={() => setIndex(3)}>4</button>
+        <button onClick={() => setIndex(prev => prev + 1)} disabled={index === 3}>&gt;</button>
+      </div>
+      <div className="center">
+        currentIndex: {index + 1}
       </div>
     </div>
   )
